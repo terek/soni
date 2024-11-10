b9328c19f144fad2462891d9f202903f2f9c378f
Zsolt <zsolt@intense3>
2024-11-10 21:27:14 +0100

    more levels

--- src/levels.ts
+++ src/levels.ts
@@ -96,6 +96,102 @@ export const CHAPTER_DATA: Record<string, ChapterData> = {
       { id: "repulo", name: "repülő" },
     ],
   },
+  "05": {
+    id: "05",
+    title: "Emberek 1",
+    numRows: 2,
+    numCols: 4,
+    classNames:
+      "portrait:grid-cols-2 portrait:grid-rows-4 landscape:grid-cols-4 landscape:grid-rows-2",
+    names: [
+      { id: "siras", name: "sírás" },
+      { id: "kohoges", name: "köhögés" },
+      { id: "nevetes", name: "nevetés" },
+      { id: "horkolas", name: "horkolás" },
+
+      { id: "indiankialtas", name: "indíánkiáltás" },
+      { id: "eves", name: "evés" },
+      { id: "asitas", name: "áshatás" },
+      { id: "tapsolas", name: "tapsolás" },
+    ],
+  },
+  "06": {
+    id: "06",
+    title: "Emberek 2",
+    numRows: 2,
+    numCols: 4,
+    classNames:
+      "portrait:grid-cols-2 portrait:grid-rows-4 landscape:grid-cols-4 landscape:grid-rows-2",
+    names: [
+      { id: "puszi", name: "puszi" },
+      { id: "jodlizas", name: "jodlizás" },
+      { id: "liheges", name: "lihegés" },
+      { id: "tusszentes", name: "tüsszentés" },
+
+      { id: "szurcsoles", name: "szürcsölés" },
+      { id: "almaharapas", name: "almaharapás" },
+      { id: "sikitas", name: "síkítás" },
+      { id: "futtyentes", name: "futtyogás" },
+    ],
+  },
+  "07": {
+    id: "07",
+    title: "Helyek (természet)",
+    numRows: 2,
+    numCols: 3,
+    classNames:
+      "portrait:grid-cols-2 portrait:grid-rows-3 landscape:grid-cols-3 landscape:grid-rows-2",
+    names: [
+      { id: "tengerpart", name: "tengerpart" },
+      { id: "nadas", name: "nádas" },
+      { id: "baromfiudvar", name: "baromfiudvar" },
+
+      { id: "utca", name: "utca" },
+      { id: "erdo", name: "erdő" },
+      { id: "dzsungel", name: "dzsungel" },
+    ],
+  },
+  "08": {
+    id: "08",
+    title: "Helyek (város)",
+    numRows: 2,
+    numCols: 3,
+    classNames:
+      "portrait:grid-cols-2 portrait:grid-rows-3 landscape:grid-cols-3 landscape:grid-rows-2",
+    names: [
+      { id: "iskolaudvar", name: "iskolaudvar" },
+      { id: "etterem", name: "étterem" },
+      { id: "szinhaz", name: "színház" },
+
+      { id: "buszpalyaudvar", name: "buszpályaudvar" },
+      { id: "stadium", name: "stadion" },
+      { id: "cirkusz", name: "cirkusz" },
+    ],
+  },
+  "09": {
+    id: "09",
+    title: "Víz",
+    numRows: 3,
+    numCols: 4,
+    classNames:
+      "portrait:grid-cols-3 portrait:grid-rows-4 landscape:grid-cols-4 landscape:grid-rows-3",
+    names: [
+      { id: "jegkocka", name: "jégkocka" },
+      { id: "vihar", name: "vihar" },
+      { id: "csobbanas", name: "csobbanás" },
+      { id: "kezmosas", name: "kezmosás" },
+
+      { id: "buvar", name: "búvár" },
+      { id: "eso", name: "eső" },
+      { id: "zuhanyozas", name: "zuhanyozás" },
+      { id: "WC_oblites", name: "WC-öblítés" },
+
+      { id: "vizeses", name: "vízesés" },
+      { id: "buborekolas", name: "buborékolás" },
+      { id: "hullamveres", name: "hullámverés" },
+      { id: "szoda", name: "szóda" },
+    ],
+  },
 }
 
 export function getChapters(): Array<ChapterData> {
